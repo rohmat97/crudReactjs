c660a1bacb5f73b28520fa6a346c1debbfaff79d
macbook <macbook@RohmatD.local>
2019-12-03 00:55:52 +0700

    update

---
+++ build/precache-manifest.7341ffb1357f81eb7001d57a7955477d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7edb61002b7eaf2875490a219832a1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "849747d6f8e197eb4096",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "eb25b8633cb293685fd1",
+    "url": "/static/js/2.e3b22735.chunk.js"
+  },
+  {
+    "revision": "849747d6f8e197eb4096",
+    "url": "/static/js/main.3857355d.chunk.js"
+  },
+  {
+    "revision": "fdea826beaa4a00a53e3",
+    "url": "/static/js/runtime-main.a4e3d889.js"
+  }
+]);
